36b207f79116e0841d3a5c12ca7399e8320a8a15
Lo Ferris <loferris@google.com>
2022-09-14 22:21:16 -0700

    first pass mvp

--- src/v1/helpers_sandbox.ts
+++ src/v1/helpers_sandbox.ts
@@ -1,18 +1,38 @@
-const createWriterSchema = () => {
+// import gax = require("google-gax");
+const customer_record_pb = require('../../samples/customer_record_pb.js');
 
+
+/*const createWriterSchema = (protoPath, filename) => {
+    gax.GrpcClient._resolveFile();
+}*/ //not MVP
+
+export const createSerializedRows: Uint8Array[] = (rowData: any[]) => {
+    let serializedRows: any = [];
+    rowData.forEach(entry => {
+      serializedRows.push(entry.serializeBinary())
+    })
+    return serializedRows;
 }
 
-const createSerializedRows = () => {
-
-}
-
-const createParent = (projectId: string, datasetId: string, tableId: string): string => {
+export const createParent = (projectId: string, datasetId: string, tableId: string): string => {
     const parent = `projects/${projectId}/datasets/${datasetId}/tables/${tableId}`;
     return parent;
 }
 
-const setClientOptions = () => {
+// test serialize rows = works
+// Row 1
+let row1 = new customer_record_pb.CustomerRecord();
+row1.row_num = 1;
+row1.setCustomerName('Lovelace');
 
-}
+// Row 2
+let row2 = new customer_record_pb.CustomerRecord();
+row2.row_num = 2;
+row2.setCustomerName('Turing');
 
-console.log(createParent("my-project", "my-dataset", "my-table"))
+let rowData: any = [row1, row2];
+
+console.log(createSerializedRows(rowData));
+
+// test parent path = works
+console.log(createParent("my-project", "my-dataset", "my-table")) // works
