bfb72796b5044f2eeec6d1713463cf72f2f6c9d3
Alvaro Viebrantz <aviebrantz@google.com>
2024-01-17 13:44:42 -0400

    fix: protobuf import

--- src/managedwriter/json_writer.ts
+++ src/managedwriter/json_writer.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {protobuf} from 'google-gax';
+import * as protobuf from 'protobufjs';
 import * as protos from '../../protos/protos';
 import {PendingWrite} from './pending_write';
 import {StreamConnection, RemoveListener} from './stream_connection';
