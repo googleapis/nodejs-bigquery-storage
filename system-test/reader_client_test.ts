93e906d9d368b1c7b19937d12f5267c2db1d4b5e
Daniel Bruce <djbruce@google.com>
2026-03-02 15:39:12 -0500

    eliminate imports

--- system-test/reader_client_test.ts
+++ system-test/reader_client_test.ts
@@ -24,17 +24,11 @@ import {ClientOptions} from 'google-gax';
 import * as customerRecordProtoJson from '../samples/customer_record.json';
 import * as bigquerystorage from '../src';
 import * as reader from '../src/reader';
-import {ReadSession} from '../src/reader/read_session';
 import {cleanupDatasets} from './util';
 import {RecordBatch, Table, tableFromIPC} from 'apache-arrow';
-import {
-  ArrowRawTransform,
-  ArrowRecordBatchTableRowTransform,
-  ArrowRecordBatchTransform,
-  ArrowRecordReaderTransform,
-} from '../src/reader/arrow_transform';
+import {ArrowRecordBatchTableRowTransform} from '../src/reader/arrow_transform';
 import {ResourceStream} from '@google-cloud/paginator';
-import {ArrowTableReader, TableReader} from '../src/reader';
+import {ArrowTableReader} from '../src/reader';
 
 type ReadRowsResponse =
   protos.google.cloud.bigquery.storage.v1.IReadRowsResponse;
